e40f2ed572a08253c1605526a9ff2a86f2010dc6
omfg <podsh74@gmail.com>
2017-12-06 23:45:27 +0500

    123

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,14 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/main.fxml"));
         primaryStage.setTitle("Hello World");
+        primaryStage.setMinWidth(400);
+        primaryStage.setMinHeight(600);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        Button add = new Button();
+
     }
 
 
